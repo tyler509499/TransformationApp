c93ba1deadfd19961976e4ca5c6372c771ba6e7c
Nikita Galkov <n.galkov@growapp.biz>
2021-08-15 21:07:29 +0300

    start anglel fix

--- TransformationApp/ViewController.swift
+++ TransformationApp/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 class ViewController: UIViewController {
     
     private lazy var rectangle = UIView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
-    private var lastRotation: CGFloat = 0
-
+    
+    var startPoint = CGPoint()
+    var currentPoint = CGPoint()
+    var startAngle = CGFloat()
+    var currentAngle = CGFloat()
+    var currentRotation = CGFloat()
+    var totalRotation = CGFloat()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         viewLoaded()
@@ -25,11 +31,24 @@ class ViewController: UIViewController {
                                      y: view.frame.size.height / 2)
     }
     
+    func angle(_ point:CGPoint) -> CGFloat{
+        let angle = atan2(point.y - (self.view?.center.y)!, point.x - (self.view?.center.x)!)
+        return angle
+    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-        let location = touch.location(in: view)
-        let angle = atan2(rectangle.center.y-location.y, rectangle.center.x-location.x)
-        rectangle.transform = CGAffineTransform(rotationAngle: angle)
+        
+        startPoint = touch.previousLocation(in: self.view)
+        currentPoint = touch.location(in: self.view)
+        
+        startAngle = angle(startPoint)
+        currentAngle = angle(currentPoint)
+        
+        currentRotation = currentAngle - startAngle
+        totalRotation += currentRotation
+        
+        self.rectangle.transform = CGAffineTransform(rotationAngle: totalRotation)
     }
 }
 
